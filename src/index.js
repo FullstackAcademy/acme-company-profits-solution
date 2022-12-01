2b9152a70d1d4291e64bd71bd3f1fee02f6811b7
ericpkatz <ericpkatz@gmail.com>
2022-12-01 10:50:19 -0500

    added webpack

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Companies from './Companies';
+import Profits from './Profits';
+
+const BASE_URL = 'https://www.acme-api.com/api'; 
+const COMPANIES_URL = `${BASE_URL}/companies`;
+
+const App = () => {
+  const { useState, useEffect } = React;
+  const [companies, setCompanies] = useState([]);
+  const [profits, setProfits ] = useState([]);
+
+  const loadCompanies = async()=> {
+    const response = await fetch(COMPANIES_URL);
+    const json = await response.json();
+    setCompanies(json);
+  }
+
+  const loadProfits = async()=> {
+    const id = window.location.hash.slice(1);
+    if(!id){
+      setProfits([]);
+    }
+    else {
+      const response = await fetch(`${COMPANIES_URL}/${id}/companyProfits`)
+      const json = await response.json();
+      setProfits(json);
+    }
+  }
+
+  useEffect(()=> {
+    loadCompanies();
+    window.addEventListener('hashchange', loadProfits)
+    loadProfits();
+  }, []);
+  return (
+    <div>
+      <h1><a href='#'>Acme Companies ({ companies.length })</a></h1>
+      <main>
+        <Companies companies={ companies } />
+        <Profits profits={ profits }/>
+      </main>
+    </div>
+  );
+}
+
+let root = ReactDOM.createRoot(document.querySelector('#app'));
+root.render(<App />);
